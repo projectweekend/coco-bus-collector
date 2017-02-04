a9ed56069409e63930b58b4c587983b4ca000868
Brian Hines <brian@projectweekend.net>
2017-02-04 08:38:40 -0600

    Switch to terraform

---
+++ coco-bus-collector.tf
@@ -0,0 +1,81 @@
+variable "CTA_BUS_API_KEY" {}
+variable "CTA_BUS_PREDICTION_ROUTE" {}
+
+
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+
+data "aws_iam_policy_document" "coco_lambda_assume_role_policy" {
+    statement {
+        actions = [ "sts:AssumeRole" ]
+        principals {
+            type = "Service"
+            identifiers = ["lambda.amazonaws.com"]
+        }
+    }
+}
+
+
+resource "aws_dynamodb_table" "coco_bustracker_dynamodb_table" {
+    name = "coco_bustracker"
+    read_capacity = 1
+    write_capacity = 2
+    hash_key = "stop_id"
+    range_key = "current_time"
+    attribute {
+        name = "stop_id"
+        type = "S"
+    }
+    attribute {
+        name = "current_time"
+        type = "N"
+    }
+}
+
+
+resource "aws_iam_role" "coco_bustracker_lambda_iam_role" {
+    name = "coco_bustracker_lambda"
+    assume_role_policy = "${data.aws_iam_policy_document.coco_lambda_assume_role_policy.json}"
+}
+
+
+resource "aws_lambda_function" "coco_bustracker_lambda_17772" {
+    filename = "./deploy/lambda.zip"
+    function_name = "coco_bustracker_stop_17772"
+    role = "${aws_iam_role.coco_bustracker_lambda_iam_role.arn}"
+    handler = "main.lambda_handler"
+    runtime = "python2.7"
+    timeout = 10
+    source_code_hash = "${base64sha256(file("./deploy/lambda.zip"))}"
+    environment {
+        variables = {
+            CTA_BUS_API_KEY = "${var.CTA_BUS_API_KEY}"
+            CTA_BUS_PREDICTION_ROUTE = "${var.CTA_BUS_PREDICTION_ROUTE}"
+            CTA_BUS_STOP_ID = "17772"
+            DYNAMODB_TABLE = "${aws_dynamodb_table.coco_bustracker_dynamodb_table.name}"
+        }
+    }
+}
+
+
+resource "aws_cloudwatch_event_rule" "coco_every_5_minutes" {
+    name = "coco_every_5_minute"
+    schedule_expression = "rate(5 minutes)"
+}
+
+
+resource "aws_cloudwatch_event_target" "coco_bustracker_lambda_17772_sched" {
+    rule = "${aws_cloudwatch_event_rule.coco_every_5_minutes.name}"
+    arn = "${aws_lambda_function.coco_bustracker_lambda_17772.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_coco_bustracker_lambda_17772" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.coco_bustracker_lambda_17772.function_name}"
+    principal = "events.amazonaws.com"
+    source_arn = "${aws_cloudwatch_event_rule.coco_every_5_minutes.arn}"
+}
